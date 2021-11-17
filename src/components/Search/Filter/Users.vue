0f80d80c87d9c29fc0e611e1f592926b26c2010f
Ruslan Melnyk <ruslan_melnyk@ukr.net>
2021-11-17 21:38:46 +0200

    adding select location

--- src/components/Search/Filter/Users.vue
+++ src/components/Search/Filter/Users.vue
@@ -15,21 +15,30 @@
     .search-filter__block.region
         label.search__label Регион:
         .search__row
-            select.select.search-filter__select(v-model='country')
-                option(value='null') Страна
-                option(v-for='country in getCountries', :key='country.id') {{ country.title }}
+            select-location.search-filter__select(
+              v-model="country",
+              placeholder='Страна',
+              :options-list="countries"
+              @selectOoption="setCountry"
+            )
 
-            select.select.search-filter__select(v-model='city')
-                option(value='null') Город
-                option(v-for='city in getCityFilter', :key='city.countryId') {{ city.country }}
+            select-location.search-filter__select(
+              v-model="city",
+              placeholder='Город',
+              :options-list="cities"
+              @selectOoption="setCity"
+            )
     .search-filter__block.btn-news(@click.prevent='clerAndSearchUser')
         button-hover Применить
 </template>
 
 <script>
 import { mapGetters, mapActions, mapMutations } from 'vuex'
+import SelectLocation from '@/components/FormElements/SelectLocation.vue'
+
 export default {
   name: 'SearchFilterUsers',
+  components: { SelectLocation },
   data: () => ({
     first_name: null,
     last_name: null,
@@ -42,6 +51,20 @@ export default {
   }),
   computed: {
     ...mapGetters('profile/country_city', ['getCountries', 'getCities']),
+    countries() {
+      return this.getCountries.data || [];
+    },
+    cities() {
+      return this.getCities.data || [];
+    },
+    selectedCountryId() {
+      const country = this.countries.find(
+        (item) => item.title.toUpperCase() === this.country.toUpperCase()
+      )
+
+      console.log('country: ', country);
+      return country ? country.id : 0;
+    },
     getCityFilter() {
       if (!this.country || this.country === 'null') {
         return this.getCities
@@ -50,10 +73,27 @@ export default {
       }
     }
   },
+
+  created() {
+    this.apiCountries()
+    this.apiCities({ countryId: 1 })
+  },
+  watch: {
+    country(newVal) {
+      this.apiCountries({ country: newVal });
+      this.apiCities({ countryId: this.selectedCountryId });
+    },
+    city(newVal) {
+      this.apiCities({
+        countryId: this.selectedCountryId,
+        city: newVal,
+      });
+    }
+  },
   methods: {
     ...mapActions('global/search', ['searchUsers']),
     ...mapMutations('global/search', ['setOffsetUsers']),
-    ...mapActions('profile/country_city', ['apiCountries', 'apiAllCities', 'clearSearchUsers']),
+    ...mapActions('profile/country_city', ['apiCountries', 'apiCities', 'clearSearchUsers']),
     onSearchUsers() {
       let { first_name, last_name, age_from, age_to, country, city } = this
 
@@ -72,18 +112,47 @@ export default {
       setTimeout(() => {
         this.onSearchUsers()
       }, 0)
+    },
+    setCountry(countryData) {
+      this.country = countryData.title;
+    },
+    setCity(value) {
+      this.city = value.title
     }
   },
-  created() {
-    this.apiCountries()
-    this.apiAllCities()
-  },
-  watch: {
-    city(value) {
-      if (!value || value === 'null') return
-      const countryId = this.getCities.find(el => el.country === value).cityId
-      this.country = this.getCountries.find(el => el.id === countryId).title
+}
+</script>
+
+
+<style lang="stylus">
+.search-filter {
+  flex-wrap: wrap;
+  &__block {
+    flex: 0 0 50%;
+    max-width: 300px;
+  }
+  &__block.age {
+    max-width: 300px;
+  }
+  &__block.region {
+    flex: 0 0 auto;
+    max-width: 100%;
+    width: auto;
+    margin-top 10px;
+    margin-left 0;
+
+    .search-filter__select {
+      max-width: 300px;
+      width: 300px;
+
+      &:first-child {
+        margin-right 20px;
+      }
+
+      .select-location__input {
+        border-color: #cbcbcb;
+      }
     }
   }
 }
-</script>
+</style>
